04baa436d66449b6e3f048774ec7a8e251204e4e
ManavSaini124 <itsmemanavsaini@gmail.com>
2025-06-12 23:49:46 +0530

    hate my life

--- app/main.js
+++ app/main.js
@@ -56,6 +56,20 @@ const parting =(answer)=>{
   const args = answer.split(" ");
   return args;
 }
+
+const exit = (args) => {
+  if (args[0] === "exit" && args[1] === "0") {
+    rl.close();
+    process.exit(0);
+  }
+};
+
+const echo = (args) => {
+  // Join the arguments with a space and print them
+   if(args[0] == "echo"){
+      console.log(args.slice(1).join(" "));
+   }
+};
 const unexpected = (args) => {
   // If the first argument is not "echo" or "exit", print an error message
   if (!builtins[command]) {
@@ -66,24 +80,48 @@ const unexpected = (args) => {
   }
 };
 
-const externalCommand = (args) => {
-  if(args.length > 1){
-    const command_args = args.slice(1);
-    // spawnSync(args[0], command_args, {
-    //   stdio: 'inherit',
-    //   encoding: 'utf8',
-    // });
-  const command = args[0];
-  const paths = process.env.PATH.split(":");
-
-  for (const dir of paths) {
-    const fullPath = `${dir}/${command}`;
-    if (fs.existsSync(fullPath) && fs.statSync(fullPath).isFile()) {
-      const proc = spawnSync(args[0], args.slice(1), { stdio: "inherit", encoding: "utf8" });
-      return 1;
+const type = (args) => {
+  const validCommands = ["echo", "exit", "type", "pwd"];
+  if(args[0] === "type"){
+    if(validCommands.includes(args[1])){
+      console.log(`${args[1]} is a shell builtin`);
+    }
+    else{
+      if (args.length < 2) {
+        console.log("type: missing argument");
+        return;
+      }
+      
+      // access the PATH environment variable to find the command
+      const dirs = process.env.PATH.split(':');
+      for( const dir in dirs){
+        const filePath = `${dirs[dir]}/${args[1]}`;
+        if (fs.existsSync(filePath) && fs.statSync(filePath).isFile()) {
+          console.log(`${args[1]} is ${filePath}`);
+          return;
+        }
+      }
+      console.log(`${args[1]}: not found`);
     }
   }
 }
+
+const externalCommand = (args) => {
+  if(args.length > 1){
+    const command_args = args.slice(1);
+    spawnSync(args[0], command_args, {
+      stdio: 'inherit',
+      encoding: 'utf8',
+    });
+  // const command = args[0];
+  // const paths = process.env.PATH.split(":");
+
+  // for (const dir of paths) {
+  //   const fullPath = `${dir}/${command}`;
+  //   if (fs.existsSync(fullPath) && fs.statSync(fullPath).isFile()) {
+  //     const proc = spawnSync(args[0], args.slice(1), { stdio: "inherit", encoding: "utf8" });
+      return 1;
+    }
   return 0;
 }
 
