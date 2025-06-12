edb91392976ce2806f9a87c190fd45f7e6bb4447
ManavSaini124 <itsmemanavsaini@gmail.com>
2025-06-12 17:35:01 +0530

    echo added

--- app/main.js
+++ app/main.js
@@ -8,11 +8,23 @@ const rl = readline.createInterface({
 // Uncomment this block to pass the first stage
 const prompt=()=>{
   rl.question("$ ", (answer) => {
-    if(answer === "exit 0") {
+    // i want to take the input as a string and then trim it and then split it by space
+    answer = answer.trim();
+    const args = answer.split(" ");
+    // if the first argument is "exit" and the second argument is "0" then close the readline interface
+    if (args[0] === "exit" && args[1] === "0") {
       rl.close();
       process.exit(0);
     }
-    console.log(`${answer}: command not found`)
+    // if the first argument is echo the print the rest of the arguments
+    else if(args[0] == "echo"){
+      console.log(args.slice(1).join(" "));
+    }
+    
+    else{ 
+      // if any invali argument then print the command not found message
+      console.log(`${answer}: command not found`)
+    }
     prompt();
     // rl.close();
   });
