20f81db78a46aba50f030547a8efba66bf519c2e
ManavSaini124 <itsmemanavsaini@gmail.com>
2025-06-12 17:16:23 +0530

    exit command added

--- app/main.js
+++ app/main.js
@@ -8,6 +8,10 @@ const rl = readline.createInterface({
 // Uncomment this block to pass the first stage
 const prompt=()=>{
   rl.question("$ ", (answer) => {
+    if(answer === "exit 0") {
+      rl.close();
+      return;
+    }
     console.log(`${answer}: command not found`)
     prompt();
     // rl.close();
