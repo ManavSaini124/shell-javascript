087d2a192e50b099e1947c1c4734089c0f52e9a0
ManavSaini124 <itsmemanavsaini@gmail.com>
2025-06-12 22:38:57 +0530

    type expended

--- app/main.js
+++ app/main.js
@@ -1,43 +1,66 @@
 const readline = require("readline");
+const fs = require("fs");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// Uncomment this block to pass the first stage
+const parting =(answer)=>{
+  answer = answer.trim();
+  const args = answer.split(" ");
+  return args;
+}
+
+const exit = (args) => {
+  if (args[0] === "exit" && args[1] === "0") {
+    rl.close();
+    process.exit(0);
+  }
+};
+
+const echo = (args) => {
+  // Join the arguments with a space and print them
+   if(args[0] == "echo"){
+      console.log(args.slice(1).join(" "));
+   }
+};
+const unexpected = (args) => {
+  // If the first argument is not "echo" or "exit", print an error message
+  if (args[0] !== "echo" && args[0] !== "exit" && args[0] !== "type") {
+    console.log(`${args[0]}: command not found`);
+  }
+};
+
+const type = (args) => {
+  const validCommands = ["echo", "exit", "type"];
+  if(args[0] === "type"){
+    if(validCommands.includes(args[1])){
+      console.log(`${args[1]} is a shell builtin`);
+    }
+    else{
+      const dirs = process.env.PATH.split(':');
+      for( const dir in dirs){
+        const filePath = `${dirs[dir]}/${args[1]}`;
+        if (fs.existsSync(filePath) && fs.statSync(filePath).isFile()) {
+          console.log(`${args[1]} is ${filePath}`);
+          return;
+        }
+      }
+      console.log(`${args[1]}: not found`);
+    }
+  }
+}
+
+
 const prompt=()=>{
   rl.question("$ ", (answer) => {
-    // i want to take the input as a string and then trim it and then split it by space
-    answer = answer.trim();
-    const args = answer.split(" ");
-    // if the first argument is "exit" and the second argument is "0" then close the readline interface
-    if (args[0] === "exit" && args[1] === "0") {
-      rl.close();
-      process.exit(0);
-    }
-    // if the first argument is echo the print the rest of the arguments
-    else if(args[0] == "echo"){
-      console.log(args.slice(1).join(" "));
-    }
-
-    else if(args[0] == "type"){
-      if(args[1] == "echo" || args[1] == "exit" || args[1] == "type"){
-        // if the second argument is echo or exit then print the shell builtin message
-        console.log(`${args[1]} is a shell builtin`);
-      }
-      else{
-        // if the second argument is not echo or exit then print the command not found message
-        console.log(`${args[1]}: not found`);
-      }
-    }
-    
-    else{ 
-      // if any invali argument then print the command not found message
-      console.log(`${answer}: command not found`)
-    }
+    const args =parting (answer);
+    exit(args);
+    echo(args);
+    type(args);
+    unexpected(args);
     prompt();
-    // rl.close();
   });
 }
 
---
+++ app/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+lse{ 
+    //   // if any invali argument then print the command not found message
+    //   console.log(`${answer}: command not found`)
+    // }
